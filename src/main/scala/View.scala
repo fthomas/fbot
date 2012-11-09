0f5b359ea448db3540861278c8c64bf153cf343b
Frank S. Thomas <frank@timepit.eu>
2012-11-09 06:43:04 +0100

    Added offsetToFarthes.

--- src/main/scala/View.scala
+++ src/main/scala/View.scala
@@ -1,24 +1,33 @@
 case class View(cells: String) {
   val size: Int = math.sqrt(cells.length).toInt
 
-  val entities = for (cell <- cells; entity <- Entity.from(cell)) yield entity
-  def terrain = View(entities map (_.terrain) toString)
+  lazy val entities =
+    for (cell <- cells; entity <- Entity.from(cell)) yield entity
+  lazy val offsets =
+    entities.zipWithIndex.map(x => relPosFromIndex(x._2).toVec -> x._1)
 
   def apply(rel: Pos) = cells(indexFromRelPos(rel))
+
+  def offsetsToBy[B](pred: Entity => Boolean, sortFun: ((Vec, Entity)) => B)
+                    (implicit ord: Ordering[B]) = {
+    offsets.filter(x => pred(x._2)).sortBy(sortFun)
+  }
+
+  def offsetToNearest(pred: Entity => Boolean): Option[Vec] = {
+    offsetsToBy(pred, _._1.lInfLength).headOption.map(_._1)
+  }
 
   def offsetToNearest(entity: Entity): Option[Vec] =
     offsetToNearest(_ == entity)
 
-  def offsetToNearest(p: Entity => Boolean): Option[Vec] = {
-    val relativePositions =
-      entities.view
-              .zipWithIndex
-              .filter(x => p(x._1))
-              .map(x => relPosFromIndex(x._2).toVec)
-
-    if (relativePositions.isEmpty) None
-    else Some(relativePositions.minBy(_.lInfLength))
+  def offsetToFarthest(pred: Entity => Boolean): Option[Vec] = {
+    offsetsToBy(pred, _._1.lInfLength).lastOption.map(_._1)
   }
+
+  def offsetToFarthest(entity: Entity): Option[Vec] =
+    offsetToFarthest(_ == entity)
+
+  def terrain = View(entities map (_.terrain) toString)
 
   private val absCenter = Pos(size / 2, size / 2)
 

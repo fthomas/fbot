e9f6b751a5019e5dae8de1264bc6d6a22d510a39
Frank S. Thomas <frank@timepit.eu>
2012-12-04 17:34:43 +0100

    Using Spawn's other ctor.

--- src/main/scala/RandomSpawning.scala
+++ src/main/scala/RandomSpawning.scala
@@ -3,9 +3,9 @@ import scala.util.Random
 trait RandomSpawning extends NoopResponding {
   override def respondToReact(react: React): List[PluginOpcode] = {
     val res = if (Random.nextFloat > 0.75) {
-      List(Spawn(Map("direction" -> Vec.random().toString)))
+      List(new Spawn(Vec.random()))
     } else {
-      List()
+      Nil
     }
     super.respondToReact(react) ++ res
   }
